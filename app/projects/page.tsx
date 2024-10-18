2b1b1288d0b0745a28a9a7598720139b451a0cb4
Iosif <dev@iosifv.com>
2024-10-18 02:41:14 +0100

    [main] - updated projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,9 +20,12 @@ export default async function ProjectsPage() {
     return acc;
   }, {} as Record<string, number>);
 
-  const featured = allProjects.find((project) => project.slug === "unkey")!;
-  const top2 = allProjects.find((project) => project.slug === "planetfall")!;
-  const top3 = allProjects.find((project) => project.slug === "highstorm")!;
+  // List of the featured projects 
+  const featured = allProjects.find((project) => project.slug === "homeless.developer")!;
+  const top2 = allProjects.find((project) => project.slug === "casa4")!;
+  const top3 = allProjects.find((project) => project.slug === "recspenses")!;
+
+  // For loop displaying all the projects
   const sorted = allProjects
     .filter((p) => p.published)
     .filter(
