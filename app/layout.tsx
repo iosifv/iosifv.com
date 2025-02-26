b809a3551cfbdc3aa20063cc6d58329769b1e5fe
Iosif <dev@iosifv.com>
2025-02-26 12:06:41 +0000

    [main] - added cloudflare web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,12 @@ export default function RootLayout({
         }`}
       >
         {children}
+        {/* Cloudflare Web Analytics */}
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "241535b3ccab4b03b66d5edeff27c1c8"}'
+        ></script>
       </body>
     </html>
   );
